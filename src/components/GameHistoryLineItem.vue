9eabfa361633f03959f2b83ea38252b36eb32904
Marius Pop <marius@mlpinit.com>
2018-06-05 21:57:06 -0400

    highlight currently selected game

--- src/components/GameHistoryLineItem.vue
+++ src/components/GameHistoryLineItem.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="game-history-line-item">
-    <div :class="isOwner">
-      <!-- TODO place owner styles below -->
-    </div>
-    <li @click="updateGameSelection" class="list-group-item mx-auto w-lg">
-      <span>{{ game.name }}</span>
-      #
-      <span>{{ game.id }}</span>
+  <div class="game-history-line-item container">
+    <li @click="updateGameSelection" class="list-group-item mx-auto w-lg"
+      :class="{'selected-game': selected}">
+      <div class="row">
+        <div class="col">
+        </div>
+        <div class="col-xs-1">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-1">
+          <span>{{ game.id }}</span>
+        </div>
+        <div class="col">
+          <span>{{ game.name }}</span>
+        </div>
+        <div class="col-xs-1">
+          <span :class="isOwner" class="fa fa-bookmark"
+            aria-hidden="true">
+          </span>
+          <span :class="isNotOwner" class="fa fa-child"
+            aria-hidden="true">
+          </span>
+        </div>
+      </div>
     </li>
   </div>
 </template>
@@ -17,12 +34,18 @@ import { mapGetters } from 'vuex'
 export default {
   name: 'Invite',
   computed: {
-    ...mapGetters({ currentUser: 'currentUser' }),
+    ...mapGetters({ currentUser: 'currentUser', selectedGame: 'selectedGame' }),
     isOwner: function () {
       return { 'd-none': this.game.isOwner(this.currentUser) }
     },
+    isNotOwner: function () {
+      return { 'd-none': !this.game.isOwner(this.currentUser) }
+    },
     shoulHideMarks: function () {
       return { 'd-none': this.game.shoulHideMarks(this.currentUser) }
+    },
+    selected: function () {
+      return this.selectedGame === this.game
     }
   },
   props: [
