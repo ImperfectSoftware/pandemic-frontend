93485af5dbd40d2a113dc0eccca2c88469cca09b
Marius Pop <marius@mlpinit.com>
2018-06-14 08:38:57 -0500

    display error message if game could not be started

--- src/services/StartGameService.js
+++ src/services/StartGameService.js
@@ -1,4 +1,5 @@
 import axios from '@/backend/vue-axios'
+import store from '.././store'
 
 export default class StartGameService {
   static call (game) {
@@ -16,7 +17,15 @@ export default class StartGameService {
   }
 
   startingGame = (data) => {
-    // TODO: add logic to redirect and start game
+    if (data.error) {
+      store.dispatch('updateError', {
+        message: data.error,
+        css: 'alert-danger',
+        display: true
+      })
+    }
+    // TODO: add logic to redirect and start game in the game subscriber if the
+    // game was started succesfully (3,2,1)
   }
 
   startingGameFailed = (e) => {
